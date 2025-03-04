fa68124d80de262a7514f6ea60712e90df631459
Patel Truptesh <55374686+truptesh96@users.noreply.github.com>
2025-03-04 18:06:39 +0530

    Update QuizJS.js
    With checkbox

--- QuizJS.js
+++ QuizJS.js
@@ -1,50 +1,74 @@
-<script>	
+
+<script>
  
 /*-------- Quiz Script ----------*/
 	 
-	var resultOneCount = 0; var resultTwoCount = 0;  var resultThreeCount = 0;
+	jQuery( function($) {
+		var resultOneCount = 0; var resultTwoCount = 0;
 	
-	jQuery(document).on('change', '.quiz-form_wrapper.gform_wrapper input[type=radio]', function() {
-		
-		jQuery(this).parents('.gchoice').addClass('vselected');
-		
-		var lastSelectedValue = jQuery(this).parents('.gchoice').siblings('.gchoice.vselected').find('input').attr('value');
-		// console.log(lastSelectedValue);
-		(lastSelectedValue === 'cosmetology') ? resultOneCount-- : '';
-		(lastSelectedValue === 'esthetics') ? resultTwoCount-- : '';
-		(lastSelectedValue === 'hairstylist') ? resultThreeCount-- : '';
-		
-		jQuery(this).parents('.gchoice').addClass('vselected').siblings('.gchoice').removeClass('vselected');
-		
-		var resultVoteLabel = jQuery(this).attr('value');  
-		(resultVoteLabel === 'cosmetology') ? resultOneCount++ : '';
-		(resultVoteLabel === 'esthetics') ? resultTwoCount++ : '';
-		(resultVoteLabel === 'hairstylist') ? resultThreeCount++ : '';
-		var resultFinal = [["cosmetology Votes", resultOneCount],["esthetics Votes",resultTwoCount],["hairstylist Votes",resultThreeCount]];
-		console.table(resultFinal);	
-	
-	});
-		
+		jQuery(document).on('change', '.quiz-form_wrapper.gform_wrapper input[type=radio]', function() {
 
-	jQuery(document).on('mouseenter', '#gform_submit_button_10', function(){
-		var labelsArr = ['cosmetology', 'esthetics', 'hairstylist'];
-		var arrMax = [resultOneCount,resultTwoCount, resultThreeCount];
-		var maxElem = Math.max(...arrMax);
-		var arrMaxIndex = arrMax.indexOf(maxElem);
-		// console.log(arrMaxIndex);
-		 jQuery('#input_10_41').val(labelsArr[arrMaxIndex]);
-		localStorage.setItem("maxVotes", labelsArr[arrMaxIndex]);
-		
-	});
-	
-	// Show or hide elements based on the new value
-	
-	var showResultBlock = '.'+localStorage.getItem('maxVotes');
-	jQuery(showResultBlock).show();
+			jQuery(this).parents('.gchoice').addClass('vselected');
 
-		
-	/*-------- Quiz Script Ends ----------*/
- 
- 
+			var lastSelectedValue = jQuery(this).parents('.gchoice').siblings('.gchoice.vselected').find('input').attr('value');
+			console.log(lastSelectedValue);
+			(lastSelectedValue === 'Cosmetology') ? resultOneCount-- : '';
+			(lastSelectedValue === 'Esthetics') ? resultTwoCount-- : '';
 
+			jQuery(this).parents('.gchoice').addClass('vselected').siblings('.gchoice').removeClass('vselected');
+
+			var resultVoteLabel = jQuery(this).attr('value');  
+			(resultVoteLabel === 'Cosmetology') ? resultOneCount++ : '';
+			(resultVoteLabel === 'Esthetics') ? resultTwoCount++ : '';
+
+			var resultFinal = [["Cosmetology Votes", resultOneCount],["Esthetics Votes",resultTwoCount]];
+			console.table(resultFinal);	
+
+		});
+		
+		
+		jQuery(document).on('change', '.quiz-form_wrapper.gform_wrapper input[type=checkbox]', function() {
+
+			jQuery(this).parents('.gchoice').toggleClass('vselected');
+
+			var lastSelectedValue = jQuery(this).parents('.gchoice').find('input').attr('value');
+			var resultVoteLabel = jQuery(this).attr('value');  
+			
+			if ( !jQuery(this).parents('.gchoice').hasClass('vselected') ) {
+				(lastSelectedValue === 'Cosmetology') ? resultOneCount-- : '';
+				(lastSelectedValue === 'Esthetics') ? resultTwoCount-- : '';			
+			} else {
+				(resultVoteLabel === 'Cosmetology') ? resultOneCount++ : '';
+				(resultVoteLabel === 'Esthetics') ? resultTwoCount++ : '';			
+			}
+	
+			var resultFinal = [["Cosmetology Votes", resultOneCount],["Esthetics Votes",resultTwoCount]];
+			console.table(resultFinal);	
+
+		}); 
+
+		jQuery(document).on('mouseenter', '.quiz-form_wrapper [type="submit"]', function(){
+			var labelsArr = ['Cosmetology', 'Esthetics'];
+			var arrMax = [resultOneCount,resultTwoCount];
+			var maxElem = Math.max(...arrMax);
+			var arrMaxIndex = arrMax.indexOf(maxElem);
+			 console.log(arrMaxIndex);
+			 jQuery('#input_5_38').val(labelsArr[arrMaxIndex]);
+			localStorage.setItem("maxVotes", labelsArr[arrMaxIndex]);
+
+		});
+
+		// Show or hide elements based on the new value
+
+		var showResultBlock = '.'+localStorage.getItem('maxVotes');
+		jQuery(showResultBlock).show();
+
+
+		/*-------- Quiz Script Ends ----------*/
+		
+		
+	})
+	
+  
+	
 </script>
